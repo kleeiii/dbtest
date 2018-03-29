f521744ac0e792a97ac06b71e3eb2a32a8cea1d4
marselkasa <37622092+marselkasa@users.noreply.github.com>
2018-03-29 23:33:50 +0200

    ikona e ndrryshuar

--- src/pages/hospital-map/hospital-map.ts
+++ src/pages/hospital-map/hospital-map.ts
@@ -1,6 +1,7 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+
 declare var google;
 
 @IonicPage()
@@ -9,8 +10,8 @@ declare var google;
   templateUrl: 'hospital-map.html',
 })
 export class HospitalMapPage {
-
-
+  image: any;
+  
   @ViewChild('map') mapElement: ElementRef;
   map: any;
 
@@ -20,41 +21,52 @@ export class HospitalMapPage {
 
   ionViewDidLoad() {
     this.initMap();
+    
   }
 
   // loadMap() {
 
   //   let latLng = new google.maps.LatLng(41.3188151, 19.8112196);
   //   var uluru = {lat: 41.3188151, lng: 19.8112196};
-    
+
   //   let mapOptions = {
-      
+
   //     position: uluru,
   //     center: uluru,
   //     zoom: 16,
   //     mapTypeId: google.maps.MapTypeId.ROADMAP
-      
+
   //   }
 
   //   this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
 
   // }
 
-  //tn me shif
-  //Firebase ka ca metoda te vetat e po te lexosh dokumentacionin ne firebase.google.com per javascriptin do e kuposh 
-  //dokumentacjoni eshte ketu
-  //sepse ai i vendos  marekrat me ok
-  //po neve do te na autogjenerohen 
 
- initMap() {
-  var uluru = {lat: 41.3188151, lng: 19.8112196};
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 18,
-    center: uluru
-  });
-  var marker = new google.maps.Marker({
-    position: uluru,
-    map: map
-  });
-}
+
+  initMap() {
+    var uluru = { lat: 41.3188151, lng: 19.8112196 };
+    //ndrysgon 
+    
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 18,
+      center: uluru
+
+    });
+
+
+    
+    this.image = 'assets/icon/custom-marker.png'
+    var marker = new google.maps.Marker({
+      position: uluru,
+      icon: this.image,
+      map: map
+    });
+
+
+
+    
+
+  
+  }
 }
